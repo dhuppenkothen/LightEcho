b5e28aa5907731eb90d50e11c3cb8b62a12c4e10
Daniela Huppenkothen <daniela.huppenkothen@nyu.edu>
2015-05-15 10:50:14 -0700

    refactored some variables and changed some syntax to match with scikit-learn API standards. Untested

--- echostate.py
+++ echostate.py
@@ -21,15 +21,18 @@ def sigmoid(x):
 class EchoStateNetwork(object):
 
 
-    def __init__(self, x,y, N, a, r, b=None,topology="scr"):
+    def __init__(self, N = 100, a = 0.75, r = 0.1, n_washout=100,
+                 scaling=1.0, lamb=None, seed=20150513, b=None,topology="scr"):
         """
         Initialization for the echo state network for time series.
-        :param x: x-coordinate (time bins)
-        :param y: data (K by D, where K is the number of data points, D the dimensionality)
         :param N: number of hidden units.
         :param lamb: regularization term
         :param a: absolute value of input weights
         :param r: weights for forward connections between hidden weights
+        :param n_washout: number of samples in the time series to wash out
+        :param scaling: scaling between current and previous time step
+        :param lamb: regularization parameter for ridge regression
+        :param seed: seed for the random number generator
         :param b: weights for backward connections between hidden weights (for topology="dlrb" only)
         :param topology: reservoir topology (one of "SCR", "DLR", "DLRB",
                 see Rodan+Tino, "Minimum Complexity Echo State Network" for details
@@ -37,24 +40,6 @@ class EchoStateNetwork(object):
         """
 
 
-        ## x-coordinate
-        self.x = np.atleast_2d(x)
-
-        ## y-coordiante
-        self.y = np.atleast_2d(y)
-        print("shape of data stream: " + str(self.y.shape))
-
-        ## number of data points
-        self.K = int(self.y.shape[1])
-        print("Number of data points: %i"%self.K)
-
-        ## number of dimensions
-        if len(self.y.shape) > 1:
-            self.D = int(self.y.shape[0])
-        else:
-            self.D = 1
-
-        print("Dimensionality of the data: %i"%self.D)
 
         ## number of hidden units
         self.N = int(N)
@@ -68,14 +53,24 @@ class EchoStateNetwork(object):
         ## input unit weights
         self.a = a
 
-        ## initialize input weights
-        self.vv = self._initialize_input_weights(self.a)
+
+        ## number of "washout" samples
+        self.n_washout = n_washout
+
+        ## Scaling between current and pervious time step
+        self.scaling = scaling
+
+        ## Regularization parameter
+        self.lamb = lamb
+
+        ## seed for the random number generator
+        self.seed = seed
 
         ## initialize hidden weights
         self.uu = self._initialize_hidden_weights(self.r,self.b,self.topology)
 
 
-    def _initialize_input_weights(self, a):
+    def _initialize_input_weights(self, a, seed=20150513):
         """
         Initialize input weights.
         Input layer fully connected to reservoir, weights have same absolute value a,
@@ -89,7 +84,7 @@ class EchoStateNetwork(object):
         pr = 0.5
 
         ## set the seed deterministically
-        np.random.seed(seed=20150513)
+        np.random.seed(seed=seed)
 
         ## initialize weight matrix with Bernoulli distribution
         vv = scipy.stats.bernoulli.rvs(pr, size=(self.N, self.D)).astype(np.float64)
@@ -142,99 +137,104 @@ class EchoStateNetwork(object):
 
         return uu
 
-    def train(self, n_washout=100, scaling=1.0, lamb=None):
+    def fit(self,X):
         """
-        not sure I'm doing the right thing in this method!!!!
+        Fit method for the Echo State Network.
 
-        :param ww_init: initial weights to use
-        :param n_washout: number of samples in the time series to wash out
+        :param X: data (K by D, where K is the number of data points, D the dimensionality)
+
         :return:
         """
 
-        self.n_washout = n_washout
-        ## not sure this is right?
-        X = np.zeros((self.K, self.N))
-        #print("X.shape: " + str(X.shape))
-        ## set first row with random numbers between -1 and 1
-        X[0,:] = np.random.uniform(-1,1,size=(self.N))
-        #print("X[0,:]: " + str(X[0,:]))
 
-        self.X = X
+
+        ## data
+        self.X = np.atleast_2d(X)
+        print("shape of data stream: " + str(self.y.shape))
+
+        ## number of data points
+        self.K = int(self.y.shape[1])
+        print("Number of data points: %i"%self.K)
+
+        ## number of dimensions
+        if len(self.y.shape) > 1:
+            self.D = int(self.y.shape[0])
+        else:
+            self.D = 1
+
+        print("Dimensionality of the data: %i"%self.D)
+
+
+        ## initialize input weights
+        self.vv = self._initialize_input_weights(self.a)
+
+
+        ## initialize hidden units
+        H = np.zeros((self.K, self.N))
+
+        ## set first row with random numbers between -1 and 1
+        H[0,:] = np.random.uniform(-1,1,size=(self.N))
+
         ## compute activations for every time step
         ## include a scaling for memory from previous time step
         for i in xrange(self.K-1):
-            first_part = np.dot(X[i,:],self.uu)
-            #print("first part: " + str(first_part.shape))
-            second_part = np.dot(self.vv,self.y[0,i]).T
-            #print("second part: " + str(second_part.shape))
+            ## split equation into two parts to avoid stupid mistakes
+            first_part = np.dot(H[i,:],self.uu)
+            second_part = np.dot(self.vv,self.X[0,i]).T
             act = np.tanh(first_part + second_part)
-            #print("act: " + str(act))
-            X[i+1,:] = (1.0-scaling)*X[i,:] + scaling*act
+            H[i+1,:] = (1.0-self.scaling)*H[i,:] + self.scaling*act
 
 
         ## discard washout period
-        X = X[n_washout:,:]
-        print("X.shape: " + str(X.shape))
-        #vv = self.vv[n_washout:]
-        #ww = self.ww[n_washout:]
-        #ww_init = ww_init[n_washout:]
-        yy = self.y[:,n_washout:].T
+        H = H[self.n_washout:,:]
+        print("H.shape: " + str(H.shape))
 
+        yy = self.X[:,self.n_washout:].T
         print("yy.shape: " + str(yy.shape))
-        #cf = lambda weights: self._cost_function(yy, X, weights, self.lamb)
-
-        ## pseudoinverse of data, such that I can fit a linear model.
-        ## NOT SURE I NEED THIS!
-        yy_inv = np.arctanh(yy)
 
 
         ## if regularization parameter is None, then determine by cross validation
-        if lamb is None:
+        if self.lamb is None:
             ## proposals for regularization parameters
             lamb_all = [0.1, 1., 10.]
             ## initialize Ridge Regression classifier
             rr_clf = RidgeCV(alphas=lamb_all)
             ## fit the data with the linear model
-            rr_clf.fit(X, yy)
+            rr_clf.fit(H, yy)
             ## regularization parameter determined by cross validation
             self.lamb = rr_clf.alpha_
 
         else:
-            rr_clf = Ridge(alpha=lamb)
-            rr_clf.fit(X,yy)
+            rr_clf = Ridge(alpha=self.lamb)
+            rr_clf.fit(H,yy)
 
         ## best-fit output weights
         self.ww = rr_clf.coef_
 
-        ## "simulated" data
-        yy_out = np.array([np.dot(act.T, self.ww.T) for act in X]).T
+        ## store activations for future use
+        self.H = H
 
-        return self.ww, yy_out
+        return
 
-    def test(self, testdata, scaling=1.0):
+    def predict(self, X):
         """run the esn, see what happens"""
 
 
-        X = np.zeros((len(testdata), self.N))
-        X[0,:] = self.X[-1,:]
+        H = np.zeros((len(X), self.N))
+        H[0,:] = self.H[-1,:]
 
         yy_test = []
 
-        for i in range(len(testdata)-1):
+        for i in range(len(X)-1):
 
-            yy_test.append(np.dot(X[i,:], self.ww.T))
+            yy_test.append(np.dot(H[i,:], self.ww.T))
 
-            first_part = np.dot(X[i,:],self.uu)
+            first_part = np.dot(H[i,:],self.uu)
             #print("first part: " + str(first_part.shape))
-            second_part = np.dot(self.vv,self.y[0,i]).T
+            second_part = np.dot(self.vv, X[0,i]).T
             #print("second part: " + str(second_part.shape))
             act = np.tanh(first_part + second_part)
-            X[i+1,:] = (1.0-scaling)*X[i,:] + scaling*act
-            #print("X: " + str(X[i,:]))
-
-        #weights_first_term = np.dot(self.X.T,self.X) + self.lamb**2.*np.identity(self.N)
-        #weights_second_term = np.dot(self.X.T, testdata)
-        #ww_test = np.dot(weights_first_term**-1., weights_second_term)
+            H[i+1,:] = (1.0-self.scaling)*H[i,:] + self.scaling*act
 
         return np.array(yy_test).T
 
